1efb9d7546c13554feb96e34aaff873e501a6cab
codeaftermath <17331561+codeaftermath@users.noreply.github.com>
2024-12-01 01:18:48 +0000

    Update simple node app to accept NAME var

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gha-test-env
+
+## Include `--env` to set NAME when running docker container 
+
+Example `docker run --env NAME=${NAME} IMAGE_NAME`
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+const INSTRUCTOR = process.env.NAME || "Mickey";
+
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while (true) {
+    console.log(`${INSTRUCTOR} rule!`);
+    await sleep(5000);
   }
-  
-  async function main() {
-    while(true) {
-      console.log('Containers rule!');
-      await sleep(5000);
-    }
-  }
-  
-  main();
+}
+
+main();
